cc3fba98968ae40b44418f5ef7455d5e60ca0e34
Anass 'Serdok' Lahnin <l.anass.pro@gmail.com>
2022-08-07 18:42:10 +0200

    Initial commit

--- cmake/check-submodules.cmake
+++ cmake/check-submodules.cmake
@@ -1,13 +1,17 @@
 # Init and update submodules if necessary
+message(VERBOSE "Checking if we need to initialize git submodules...")
 if (NOT EXISTS "${CMAKE_CURRENT_SOURCE_DIR}/.git/modules" OR NOT EXISTS "${CMAKE_CURRENT_SOURCE_DIR}/.gitmodules")
+    message(VERBOSE "No git submodules found.")
     return ()
 endif()
 
 find_program(GIT git)
 if (GIT-NOTFOUND)
+    message(VERBOSE "`git` executable not found.")
     return ()
 endif()
 
+message(VERBOSE "Initializing git submodules")
 execute_process(
         COMMAND ${GIT} submodule update --init --recursive
         WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
@@ -16,3 +20,5 @@ execute_process(
 if (NOT ${GIT_RESULT} EQUAL 0)
     message(FATAL_ERROR "Initializing git submodules failed with error code ${GIT_RESULT}.")
 endif()
+
+message(VERBOSE "Done.")
